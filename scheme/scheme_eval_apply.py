acf3ac2e1c6642216c9959fb8d20a5058458f630
House2008 <Housewong08@qq.com>
2026-08-01 16:34:42 +0800

    Problem 9 solved!

--- scheme/scheme_eval_apply.py
+++ scheme/scheme_eval_apply.py
@@ -61,7 +61,8 @@ def scheme_apply(procedure, args, env):
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
     elif isinstance(procedure, LambdaProcedure):
         # BEGIN PROBLEM 9
-        "*** YOUR CODE HERE ***"
+        child = procedure.env.make_child_frame(procedure.formals,args)
+        return eval_all(procedure.body,child)
         # END PROBLEM 9
     elif isinstance(procedure, MuProcedure):
         # BEGIN PROBLEM 11
