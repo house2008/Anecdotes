fd3fdaae18797d33d12b16899c40f4e7aa2e07dd
House2008 <Housewong08@qq.com>
2026-08-01 18:00:22 +0800

    Problem 14 solved!

--- scheme/scheme_forms.py
+++ scheme/scheme_forms.py
@@ -168,7 +168,10 @@ def do_cond_form(expressions, env):
             test = scheme_eval(clause.first, env)
         if is_scheme_true(test):
             # BEGIN PROBLEM 13
-            "*** YOUR CODE HERE ***"
+            if clause.rest is nil:
+                return test
+            else:
+                return eval_all(clause.rest,env)               
             # END PROBLEM 13
         expressions = expressions.rest
 
