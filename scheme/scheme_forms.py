6d2ece96a509a3e2a633558f5fd9207734a0134c
House2008 <Housewong08@qq.com>
2026-08-01 11:28:36 +0800

    Problem 4 solved!

--- scheme/scheme_forms.py
+++ scheme/scheme_forms.py
@@ -36,7 +36,9 @@ def do_define_form(expressions, env):
         # assigning a name to a value e.g. (define x (+ 1 2))
         validate_form(expressions, 2, 2) # Checks that expressions is a list of length exactly 2
         # BEGIN PROBLEM 4
-        "*** YOUR CODE HERE ***"
+        val = scheme_eval(expressions.rest.first,env)
+        env.define(expressions.first,val)
+        return expressions.first
         # END PROBLEM 4
     elif isinstance(signature, Pair) and scheme_symbolp(signature.first):
         # defining a named procedure e.g. (define (f x y) (+ x y))
